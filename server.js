2ff3a720ccfed4220acb86a0fb9041090bf38703
Yosuke Sato <waganse@gmail.com>
2023-01-11 17:50:15 +0900

    feat: :zap: init server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+// app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head></head>
+      <body>
+        <p>AAAA</p>
+      </body>
+    </html>
+  `)
+});
+
+app.listen(9090);
+
